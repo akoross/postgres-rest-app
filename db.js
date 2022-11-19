68630bfdf477a7cf842b5b70d858fb1e9cb6c1ab
Sergii <74363342+akoross@users.noreply.github.com>
2022-11-19 18:25:19 +0200

    change db settings

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: "user",
-    password: '',
-    host: "localhost",
+    user: "cplfklpliwscqy",
+    password: 'a9088b121a7a449f04cb72769357dc10a61d64aa2e29e3ba2cbbbc15d48614fa',
+    host: "ec2-54-77-40-202.eu-west-1.compute.amazonaws.com",
     port: "5432",
-    database: "user_subscriber"
+    database: "d1phglaio3ekgs"
 })
 
 
